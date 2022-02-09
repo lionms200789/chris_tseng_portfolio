103b908349747db03f8d4af11f391a8ae07b8cfb
ChrisTseng <lionms200789@gmail.com>
2022-02-09 13:50:52 +0800

    preloader onreadyStateChange 監聽事件調整

--- static/scripts/preloader.js
+++ static/scripts/preloader.js
@@ -1,20 +1,10 @@
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        const preloader = document.querySelector('.preloader-wrap');
+        preloader.style.opacity = 0;
 
-// document.onreadystatechange = function () {
-//     if (document.readyState === "complete") {
-//         const preloader = document.querySelector('.preloader-wrap');
-//         preloader.style.opacity = 0;
-
-//         setTimeout(() => {
-//             document.body.removeChild(preloader)
-//         }, 650);
-//     }
-// };
-
-window.onload = function () {
-    const preloader = document.querySelector('.preloader-wrap');
-    preloader.style.opacity = 0;
-
-    setTimeout(() => {
-        document.body.removeChild(preloader)
-    }, 650);
-}
+        setTimeout(() => {
+            document.body.removeChild(preloader)
+        }, 650);
+    }
+};
